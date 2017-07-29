388016203df84e98d3f921f439d18b09d857786a
Socola Đại Ca <tokentien@gmail.com>
2017-07-29 13:00:32 +0700

    edit download-code.php

--- action/download-code.php
+++ action/download-code.php
@@ -1,10 +1,38 @@
-<?php 
-	// $idCode = $_GET['idCode'];
-	$file_url = '../code-store/1905472106359672_Untitled.js';
-	$filename = basename($file_url);
-	$contentType= mime_content_type($filename);
-	header("Content-Type: {$contentType}");
-	header("Content-Transfer-Encoding: Binary"); 
-	header("Content-disposition: attachment; filename=\"" . $filename . "\""); 
-	readfile($file_url); // do the double-download-dance (dirty but worky)
+<?php
+		$contentTypes=array(
+		"txt" => "text/plain",
+		"html"=> "text/html",
+		"htm" => "text/html",
+		"php" => "text/plain",
+
+		"exe" => "application/octet-stream",
+		"pdf" => "application/pdf",
+		"csv" => "application/csv",
+		"zip" => "application/zip",
+		"doc" => "application/msword",
+		"xls" => "application/vnd.ms-excel",
+		"ppt" => "application/vnd.ms-powerpoint",
+
+		"gif" => "image/gif",
+		"png" => "image/png",
+		"jpeg"=> "image/jpg",
+		"jpg" => "image/jpg",
+		
+		"mp4" => "video/mp4",
+		"3gp" => "video/3gp"
+	);
+	$id = $_GET['i'];
+	$file = "../code-store/{$id}";
+	$filename = $_GET['n'];
+	// $ext = $_GET['e'];
+	// $file = $_GET['url'];
+	$format = explode('.', $filename);
+	$format = array_pop($format);
+	$contentType = array_key_exists($format, $contentTypes) ? $contentTypes[$format] : 'application/javascript';
+	header("Content-Description: File Transfer"); 
+	header("Content-Type: {$contentType}"); 
+	header("Content-Disposition: attachment; filename='" . $filename . "'"); 
+	readfile ($file);
+	exit();
+	/*http://tentcode.dev/action/download-code.php?i=rvXehrw4&n=test.js*/
 ?>
